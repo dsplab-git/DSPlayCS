e978ea1ee9f92dae5a21931caec59a77b436d1b0
dongsung.pae <paeds915@gmail.com>
2023-07-27 02:34:43 +0900

    자리표시자 확인

--- DSPlayCS/HelloWorld.cs
+++ DSPlayCS/HelloWorld.cs
@@ -15,19 +15,20 @@ namespace DSPlayCS
         /// 해당 애플리케이션의 주 진입점입니다.
         /// </summary>
         [STAThread]
-        static void Main(string[] args)
+        static void Main(string[] args) // (svm + tab + tab)
         {
             // Full Name  (Namespace.Class.Method)
             System.Console.WriteLine("HelloWorld0");
 
-            // Basis Print Form
+            // Basis Print Form (cw + tab + tab)
             Console.WriteLine("Hello World");
-
-            
 
             // Print Form
             WriteLine("Hello World2");
 
+            Console.WriteLine("{0}, {0}","Hello");
+
+            Console.WriteLine("{1},{0},{1},{0}", "첫째", "둘째");
             
 
             //Application.EnableVisualStyles();
